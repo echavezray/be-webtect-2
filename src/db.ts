dd76ddec9c5d0e7dc200e969a453ed2189ee55b9
unknown <rmechavez75156@liceo.edu.ph>
2026-01-21 13:47:56 +0800

    Activity 2 Submission

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,7 @@
 import mysql from 'mysql2/promise';
 
-// 👇 force TypeScript to allow query()
 export const db: any = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '', // your MySQL password
-  database: 'web-tech-2',
+  password: 'echavez12345'
 });
